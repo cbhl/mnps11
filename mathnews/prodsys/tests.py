954464940cb38d60dbcef80ed26d21df94475011
Michael Chang <thenewme91@gmail.com>
2011-07-03 18:55:51 +0000

    Add test to allow saving of Issue with no cover

--- mathnews/prodsys/tests.py
+++ mathnews/prodsys/tests.py
@@ -8,7 +8,8 @@ Replace this with more appropriate tests for your application.
 from django.utils import unittest
 from django.utils.unittest import TestCase
 #from django.test import TestCase
-
+from mathnews.prodsys.models import Issue
+import datetime;
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -16,3 +17,12 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class IssueTest(TestCase):
+    def test_create_issue_nocover(self):
+        """
+        Tests that an article can be created with no cover.
+        """
+        i = Issue(slug='test',volume=0,issue=0,pub_date=datetime.datetime.now(),cover_image=None,cover_caption="");
+        i.full_clean()
+        i.save()
