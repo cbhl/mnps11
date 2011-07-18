54c311694ce02e01376ef8803857d03dc216c7c2
Michael Chang <thenewme91@gmail.com>
2011-07-18 22:06:59 +0000

    Update DTD checking to also handle good and bad entities.

--- mathnews/prodsys/models.py
+++ mathnews/prodsys/models.py
@@ -19,15 +19,28 @@ class Article(models.Model):
         from django.core.exceptions import ValidationError
         from StringIO import StringIO
         from lxml import etree
+        from lxml.etree import XMLSyntaxError
+        import datetime
+        import syslog
         import os
         dtd_fh = open(settings.STATIC_ROOT + 'article.dtd');
         dtd = etree.DTD(dtd_fh);
-        root = etree.XML(self.body);
-        if not dtd.validate(root):
-            raise ValidationError("Your article body is invalid mNmL: {0}".format(dtd.error_log))#.filter_from_errors()[0]))
+        class DTDResolver(etree.Resolver):
+            def resolve(self, url, id, context):
+                syslog.syslog('resolve: {0} {1} {2}'.format(datetime.datetime.now(), url, id))
+                if (id == 'mathNEWS//article.dtd//EN'):
+                    return self.resolve_filename(settings.STATIC_ROOT + 'article.dtd',context);
+        p = etree.XMLParser(resolve_entities=True, no_network=False, dtd_validation=True, load_dtd=True)
+        p.resolvers.add(DTDResolver())
+        try:
+            root = etree.fromstring(self.body, parser=p);
+            if not dtd.validate(root):
+                raise ValidationError("Your article body is invalid mNmL: {0}".format(dtd.error_log))#.filter_from_errors()[0]))
+        except XMLSyntaxError as xmlse:
+            raise ValidationError(xmlse.message)
+        except:
+            raise;
 
-        #if ((author is None) or (not author.is_authenticated()) or (not author.is_active)):
-        #    raise ValidationError('Author is empty, inactive, or not authenticated.');
         pass
 
     def __unicode__(self):
